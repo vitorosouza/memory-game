79c1c41aa543e2dea04a214598bd51f2c4978d53
vitorosouza <vitorosouza@hotmail.com>
2022-10-27 03:15:45 -0300

    Adiciona repos

--- js/login.js
+++ js/login.js
@@ -1,22 +1,27 @@
-const input = document.querySelector('.login__input');
-const button = document.querySelector('.login__button');
-const form = document.querySelector('.login-form');
+const input = document.querySelector('.login__input'); 
+const button = document.querySelector('.login__button'); 
+const form = document.querySelector('.login--form');
 
-const validateInput = ({ target }) => {
+//Logica para tirar o estado de disabled e para validar que quando apague tudo do input, o botão fique disabled
+const validatedInput = ({ target }) => { 
   if (target.value.length > 2) {
     button.removeAttribute('disabled');
-    return;
+    return
   }
-
+  
   button.setAttribute('disabled', '');
 }
 
 const handleSubmit = (event) => {
   event.preventDefault();
   
+  //Quando enviar o formulário, vai salvar o input
+  // e mudar de página.
   localStorage.setItem('player', input.value);
-  window.location = 'pages/game.html';
+  window.location = 'pages/game.html'
+
 }
 
-input.addEventListener('input', validateInput);
+
+input.addEventListener('input', validatedInput);
 form.addEventListener('submit', handleSubmit);
